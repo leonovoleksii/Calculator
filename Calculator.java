051bc86ef9cd80d2fc67eee4554e0f4bd87dcb5b
leonov.oleksii <leonov.oleksii73@gmail.com>
2019-03-02 11:16:42 +0200

    Calculate 2 arabic numbers

--- Calculator.java
+++ Calculator.java
@@ -15,12 +15,44 @@ public class Calculator {
 		return result;
 	}
 	public void calculate() {
-
+		char charr[] = getStr().toCharArray();
+		Operand operand1 = new Operand(charr);
+		str = arrToStr(charr);
+		Operator operator = new Operator(str);
+		str = str.substring(1, str.length());
+		charr = str.toCharArray();
+		Operand operand2 = new Operand(charr);
+		System.out.println(operand2.getValue());
+		str = arrToStr(charr);
+		switch (operator.getValue()){
+			case '+':
+				result = operand1.getValue() + operand2.getValue();
+				break;
+			case '-':
+				result = operand1.getValue() - operand2.getValue();
+				break;
+			case '*':
+				result = operand1.getValue() * operand2.getValue();
+				break;
+			case '/':
+				result = operand1.getValue() / operand2.getValue();
+				break;
+		}
+	}
+	public static String arrToStr(char[] charr) {
+		String s = "";
+		for (int i = 0; i < charr.length; i++) {
+			if (charr[i] == Character.MIN_VALUE)
+				continue;
+			s += (charr[i]);
+		}
+		return s;
 	}
 
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
 		calculator.setStr();
-		System.out.println(calculator.getStr());
+		calculator.calculate();
+		System.out.println(calculator.getRes());
 	}
 }
