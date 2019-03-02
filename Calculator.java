6322422f5c254ebacbaa961efe81c6fbc87aa476
leonov.oleksii <leonov.oleksii73@gmail.com>
2019-03-02 12:41:21 +0200

    format output

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 	private String str;
 	private double result;
 	public void setStr() {
-		System.out.print("Hello! I am calculator! What would you like to calculate? Enter here: ");
+		System.out.print("Enter here: ");
 		Scanner sc = new Scanner(System.in);
 		str = sc.nextLine();
 	}
@@ -51,11 +51,35 @@ public class Calculator {
 		}
 		return s;
 	}
+	public boolean inputIsValid() {
+		boolean arabic =false, roman = false;
+		String s = getStr();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == 'I' || c == 'V' || c == 'X') 
+				roman = true;
+			else if ((int)c >= (int)'0' && (int)c <= (int)'9')
+				arabic = true;
+		}
+		return (arabic^roman);
+	}
 
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
-		calculator.setStr();
-		calculator.calculate();
-		System.out.println(calculator.getRes());
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hello! I am calculator! What would you like to calculate?");
+		while (true) {
+			calculator.setStr();
+			if (calculator.inputIsValid()) {
+				calculator.calculate();
+				System.out.println("Answer is: " + calculator.getRes());
+			} else {
+				System.out.println("Your input is invalid!");
+			}
+			System.out.println("Would you like to try again?(Print y or Y to continue)");
+			String answer = sc.next();
+			if (!answer.equals("y") && !answer.equals("Y"))
+				break;
+		}
 	}
 }
